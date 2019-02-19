7a1751a05cddbd5135fddd180c38b1a621523c26
Victoria B <viktorija.bourassa@gmail.com>
2019-02-19 19:40:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,10 +1,13 @@
 class CreateCostumeStores < ActiveRecord::Migration
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.float :price
-      t.string :size
-      t.string :image_url
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
   end
 end
 end
